bf038c6dbd52be74db76304e432f67f0555f1db8
DevroDev <devroshein.dev@gmail.com>
2020-12-29 02:06:54 +0630

    CMS

--- models/product.js
+++ models/product.js
@@ -19,7 +19,7 @@ const productSchema = new mongoose.Schema(
     description: {
       type: String,
       required: true,
-      maxlength: 2000,
+      maxlength: 999999,
       text: true,
     },
     // price: {
